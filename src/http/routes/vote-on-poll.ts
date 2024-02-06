730f14e450517bfe395f108276128fe5ca15c7a4
henriquebarros <henriquebarros@live.com>
2024-02-06 20:42:38 -0300

    Implementando rota 'voteOnPoll' no método POST para registrar votos de usuários.

---
+++ src/http/routes/vote-on-poll.ts
@@ -0,0 +1,34 @@
+import { z } from 'zod'
+import { randomUUID } from 'node:crypto'
+import { prisma } from '../../prisma/prisma'
+import { FastifyInstance } from 'fastify'
+
+export async function voteOnPoll(app: FastifyInstance) {
+  app.post('/polls/:pollId/votes', async (request, reply) => {
+    const voteOnPollBodySchema = z.object({
+      pollOptionId: z.string().uuid(),
+    })
+
+    const voteOnPollParams = z.object({
+      pollId: z.string().uuid(),
+    })
+
+    const { pollOptionId } = voteOnPollBodySchema.parse(request.body)
+    const { pollId } = voteOnPollParams.parse(request.params)
+
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.setCookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 30, // 30 dias
+        signed: true,
+        httpOnly: true,
+      })
+    }
+
+    return reply.status(201).send()
+  })
+}
